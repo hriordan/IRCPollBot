d51e9fba404c4a72b505bb631c64f0fbb2a68221
henryriordan <henry.riordan@oracle.com>
2017-11-08 17:18:30 -0600

    letting users put in their own poll ID and cleaning up help messages and putting in various todos

--- BotExceptions.py
+++ BotExceptions.py
@@ -6,3 +6,8 @@ Custom Exception Classes for vote bot errors.
 class BadVoteOption(Exception):
     """No such vote option for a given poll"""
     pass
+
+
+class BadPollIDValue(Exception):
+    """PollID is malformed or already in use"""
+    pass
--- votebot.py
+++ votebot.py
@@ -1,23 +1,25 @@
 #! /usr/bin/env python
 """
 IRC Vote/poll Bot.
-Someday.
 An HRio Production.
 """
 
 import re
 import irc.bot
 import irc.strings
-from BotExceptions import BadVoteOption
+from BotExceptions import BadVoteOption, BadPollIDValue
 
 
 class Poll(object):
     # answers should be list of questions
+    pollIDMaxLen = 10
+
     def __init__(self, question, pollID, creator, answers=[]):
+        # TODO: associate poll with channel
         self.question = question
         self.pollID = pollID
         self.creator = creator
-        self.answers = {}  # TODO: use OrderedDict?
+        self.answers = {}
         for num, answer in enumerate(answers, start=1):
             self.answers[num] = AnswerOption(answer, num)
 
@@ -25,11 +27,16 @@ class Poll(object):
         pass
 
     def voteForAnswer(self, answerEnum):
+        # TODO: track if someone (by host) has already voted in a poll.
         answer = self.answers.get(answerEnum)
         if not answer:
             raise BadVoteOption
         answer.count += 1
-        return answer # good return val?
+        return answer
+
+    def closePoll(self):
+        """close a poll down. Don't delete contents."""
+        pass
 
 
 class AnswerOption(object):
@@ -44,24 +51,25 @@ class VoteBot(irc.bot.SingleServerIRCBot):
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
         self.channel = channel
         self.polls = {}
+        # TODO: save and load polls to db.
 
     def on_nicknameinuse(self, conn, event):
         conn.nick(conn.get_nickname() + "_")
 
     def on_welcome(self, conn, event):
+        # todo: make this listen on all channels?
         conn.join(self.channel)
 
     def on_join(self, conn, event):
-        conn.notice(event.target, "Hi I'm a poll bot! Do .VoteBot help to find out about me.")
+        conn.notice(event.target, "Hi I'm a poll bot! Do .votebot help to find out about me.")
 
     def on_privmsg(self, conn, event):
-        # self.do_command(event, event.arguments[0])
         conn.privmsg(event.source.nick, "No private chat at the moment")
 
     def on_pubmsg(self, conn, event):
         # prefix should be in ".botname cmd" format
         arg = event.arguments[0].split(" ", 1)
-        if len(arg) > 1 and arg[0] == ('.%s' % self._nickname):
+        if len(arg) > 1 and arg[0].lower() == ('.%s' % self._nickname).lower():
             self.do_command(event, arg[1].strip())
         return
 
@@ -81,17 +89,22 @@ class VoteBot(irc.bot.SingleServerIRCBot):
         # TODO: move this to dispatch table structure, moving all this stuff into own funcs
         if cmd == "createpoll":
             argsError = ("Not enough arguments supplied for createpoll cmd. Needs at least one answer supplied."
-                         "Both question and answers must be in double quotes")
+                         "pollID, question,answers must be in double quotes "
+                         "Ex: .votebot createpoll \"bore\" \"who?\" \"ann?\" ")
             if not cmdArgs:
                 conn.notice(target, argsError)
                 return
             try:
-                question, answers = self.parseCreatePollArgs(cmdArgs)
+                pollID, question, answers = self.parseCreatePollArgs(cmdArgs)
             except ValueError:
                 conn.notice(target, argsError)
                 return
-            pollID = self.handleCreatePoll(question, answers, nick)
-            # args format: question (string) variable number of options
+            except BadPollIDValue as err:
+                conn.notice(target, "Invalid Poll ID %s. Must be %s chars or fewer and not already in use" %
+                                    (err.args[0], Poll.pollIDMaxLen))
+                return
+
+            self.handleCreatePoll(pollID, question, answers, nick)
             conn.notice(target, "I created a poll with ID %s" % pollID)
             self.displayPollInfo(target, pollID)
 
@@ -122,7 +135,7 @@ class VoteBot(irc.bot.SingleServerIRCBot):
             try:
                 answer = poll.voteForAnswer(voteAnswer)
             except BadVoteOption:
-                conn.notice(target, "no much option %d for poll %s" % (voteAnswer, pollID))
+                conn.notice(target, "no such option %d for poll %s" % (voteAnswer, pollID))
                 return
             conn.notice(target, "Successful Vote. Option %d: '%s' now has %d votes." % (voteAnswer, answer.answer, answer.count))
 
@@ -131,18 +144,16 @@ class VoteBot(irc.bot.SingleServerIRCBot):
 
         elif cmd == "help":
             conn.notice(target, "Commands are list, createpoll, vote, and pollinfo")
+            # TODO: help responses for each command
 
         else:
             conn.notice(target, "Not understood: " + cmd)
 
-    def handleCreatePoll(self, question, answers, creator):
+    def handleCreatePoll(self, pollID, question, answers, creator):
         """register a poll to the polls dict."""
-        # make an "ID" for the poll. creator nick + num polls for now
-        pollID = creator + str(len(self.polls))  # sure why not
         newPoll = Poll(question, pollID, creator, answers)
         self.polls[pollID] = newPoll
         print("created poll %s" % pollID)
-        return pollID
 
     def displayPollInfo(self, target, pollID):
         """msg channel/nick a given poll's info."""
@@ -152,13 +163,13 @@ class VoteBot(irc.bot.SingleServerIRCBot):
         except KeyError:
             conn.notice(target, "No such PollID %s" % pollID)
             return
-        response = "Poll ID: %(pollID)s - '%(question)s'. Options: " % {'pollID': pollID, 'question': poll.question}
+        conn.notice(target,
+                    "Poll ID: %(pollID)s - '%(question)s'. Options: " % {'pollID': pollID, 'question': poll.question})
         # get answers from poll
         for enum, answer in sorted(poll.answers.items(), key=lambda x: x[0]):
-            response += " - %(enum)s: '%(answer)s', %(votes)s votes" % {'enum': enum,
-                                                                        'answer': answer.answer,
-                                                                        'votes': answer.count}
-        conn.notice(target, response)
+            conn.notice(target, " - %(enum)s: '%(answer)s', %(votes)s votes" % {'enum': enum,
+                                                                                'answer': answer.answer,
+                                                                                'votes': answer.count})
 
     def listPolls(self, channel):
         """list all available poll IDs to a given channel"""
@@ -168,12 +179,7 @@ class VoteBot(irc.bot.SingleServerIRCBot):
             return
         conn.notice(channel, "Open Polls:")
         for pollID in self.polls.keys():
-            # response += "PollID %s: '%s'," % (pollID, self.polls[pollID].question)
             conn.notice(channel, "PollID %s: '%s'," % (pollID, self.polls[pollID].question))
-
-    def voteInPoll(self, pollID, answerEnum):
-        """vote for answer in poll"""
-        pass
 
     @staticmethod
     def parseVoteArgs(voteArgs):
@@ -194,23 +200,26 @@ class VoteBot(irc.bot.SingleServerIRCBot):
         """one argument, pollID"""
         return infoArgs.split()[0]
 
-    @staticmethod
-    def parseCreatePollArgs(pollArgs):
+    def parseCreatePollArgs(self, pollArgs):
         """
-        Poll args should come in form [question] [options](variable #). They need to be quoted.
-        Double quotes required, for now. Example: "What time is it?" "3pm" "4pm" "midnight"
+        Poll args should come in form [pollID] [question] [options](variable #). They need to be quoted.
+        Double quotes required, for now. Example: "time" "What time is it?" "3pm" "4pm" "midnight"
         """
         stringRegex = '"([^"]*)"'
         parsedArgs = re.findall(stringRegex, pollArgs)
-        if len(parsedArgs) < 2:
+        if len(parsedArgs) < 3:
             print("not enough args for createpoll cmd")  # switch to log
             raise ValueError
-        question, answers = (parsedArgs[0], parsedArgs[1:])
-        return (question, answers)
+        pollID = parsedArgs[0]
+        if len(pollID) > Poll.pollIDMaxLen or pollID in self.polls.keys():
+            print("bad pollID")
+            raise BadPollIDValue(pollID)
+        question, answers = (parsedArgs[1], parsedArgs[2:])
+        return (pollID, question, answers)
 
 
 def main():
-    debug = True # TODO: get from cmdline arg
+    debug = False # TODO: get from cmdline arg
     import sys
     if len(sys.argv) != 4:
         print("Usage: votebot <server[:port]> <channel>")
